6eb10296d830c178265f56cdf3f5f33bd1b76db7
Omar Abed <omar.a82@yahoo.com>
2024-08-22 15:06:12 +0300

    Final configration

--- currency/views.py
+++ currency/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .tasks import send_mass_emails
+
+
+def mass_email_view(request):
+    recipent = 'omar.a82@yahoo.com'
+    print('Received the request')
+    send_mass_emails(recipient=recipent)
+    print('Sent to celery')
+    return render(request, "index.html")
